bb7e5a4bf58ee04c0ae7d94666a55f9254e313b7
xzinoviou <xzinoviou@gmail.com>
2020-05-02 01:06:54 +0300

    initial commit

---
+++ src/main/java/com/xzinoviou/sbdatarest/SbDataRestApplication.java
@@ -0,0 +1,13 @@
+package com.xzinoviou.sbdatarest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbDataRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SbDataRestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xzinoviou/sbdatarest/SbDataRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xzinoviou.sbdatarest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbDataRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
